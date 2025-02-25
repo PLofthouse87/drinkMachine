ffdbcd857ebc1136be4a6453f56a15e6b1b4e91c
PLofthouse87 <paul.lofthouse@gmail.com>
2025-02-25 19:50:26 +0000

    Working version

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-alert("Hello World!")
+// alert("Hello World!")
+
+drinkOrder = (size, buttonName) => {
+    let softDrinkLabel;
+    switch(buttonName) {
+        case "cola":
+            softDrinkLabel = "Cola"
+            break;
+        case "lemon":
+            softDrinkLabel = "Lemonade"
+            break;
+        case "orange":
+            softDrinkLabel = "Orangeade"
+            break;
+        default:
+          // code block
+      }
+      return `You have ordered a ${size} of ${softDrinkLabel}`
+
+};
+
+console.log(drinkOrder("large", "cola"));
